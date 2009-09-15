0a5e078bba768b04941bbd2e83a6566aec6670ab
e.c.austin <e.c.austin@62499fce-a953-0410-b298-737fc14f0fe2>
2009-09-15 17:17:07 +0000

    Added some implied strictness to Data.Seq test in haskell

--- tests/test-queue2.hs
+++ tests/test-queue2.hs
@@ -9,13 +9,13 @@ get_words = do
 
 enqueue_all :: [String] -> Seq String -> Seq String
 enqueue_all (w:[]) q = q |> w
-enqueue_all (w:l) q = enqueue_all l (q |> w)
+enqueue_all (w:l) q = enqueue_all l $! q |> w
 
 
 main :: IO ()
 main = do
   w <- get_words
   putStrLn (let x = (enqueue_all w q) in
-             (index x ((Data.Sequence.length x) - 1)))
+              (index x ((Data.Sequence.length x) - 1)))
   where
     q = empty::Seq String
