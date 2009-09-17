071a8e8d108b81c680c763ac4498cecaa47fc245
e.c.austin <e.c.austin@62499fce-a953-0410-b298-737fc14f0fe2>
2009-09-17 21:15:10 +0000

    Changed Haskell test to use 4 queues.

--- branches/1.0/papers/resource-typing/tests/test-queue-h.hs
+++ branches/1.0/papers/resource-typing/tests/test-queue-h.hs
@@ -11,12 +11,25 @@ enqueue_all :: [String] -> Seq String -> Seq String
 enqueue_all (w:[]) q = q |> w
 enqueue_all (w:l) q = enqueue_all l $! q |> w
 
-
+requeue_all :: Int -> Seq String -> Seq String -> Seq String
+requeue_all 0 _ r = r
+requeue_all n q r = requeue_all (n - 1) q $! r |> (index q n)
 
 main :: IO ()
 main = do
   w <- get_words
+
   putStrLn (let x = (enqueue_all w q) in
-              (index x ((Data.Sequence.length x) - 1)))
+            let y = (requeue_all (Data.Sequence.length x) x r) in
+            let z = (requeue_all (Data.Sequence.length y) y s) in
+            let aa = (requeue_all (Data.Sequence.length z) z t) in
+              (index aa 1))
+{-
+  putStrLn (let x = (enqueue_all w q) in
+            (index x ((Data.Sequence.length x) - 1)))
+-}
   where
     q = empty::Seq String
+    r = empty::Seq String
+    s = empty::Seq String
+    t = empty::Seq String
